64e93321ce658c89e3b6183d1180412c1ab97369
capybarin <vladibzd@gmail.com>
2020-01-08 13:25:26 +0200

    Initial commit

---
+++ src/main/java/com/autopark/app/entities/User.java
@@ -0,0 +1,11 @@
+package com.autopark.app.entities;
+
+public class User {
+    private String name;
+    private String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
