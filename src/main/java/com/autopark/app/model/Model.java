c2171af6601b1c576dbcf1cc77e074d15e595057
capybarin <vladibzd@gmail.com>
2020-01-08 20:28:55 +0200

    MVC: model class added

--- src/main/java/com/autopark/app/model/Model.java
+++ src/main/java/com/autopark/app/model/Model.java
@@ -1,4 +1,28 @@
 package com.autopark.app.model;
 
+import com.autopark.app.entities.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Model {
+    private static Model instance = new Model();
+    private List<User> model;
+    
+    public static Model getInstance() {
+        return instance;
+    }
+
+    private Model() {
+        model = new ArrayList<>();
+    }
+
+    public void addUser(User user){
+        model.add(user);
+    }
+
+    public List<String> list(){
+        return model.stream().map(User::getName).collect(Collectors.toList());
+    }
 }
