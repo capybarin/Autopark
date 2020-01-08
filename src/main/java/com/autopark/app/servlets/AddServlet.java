fd3bbb6e364a26400086b4bd8fd15989c5685466
capybarin <vladibzd@gmail.com>
2020-01-08 21:30:35 +0200

    Some stylesheet improvements

--- src/main/java/com/autopark/app/servlets/AddServlet.java
+++ src/main/java/com/autopark/app/servlets/AddServlet.java
@@ -1,18 +1,34 @@
 package com.autopark.app.servlets;
 
 
+import com.autopark.app.entities.User;
+import com.autopark.app.model.Model;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class AddServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/add.jsp");
         requestDispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String name = req.getParameter("name");
+        String pass = req.getParameter("pass");
+        User user = new User(name, pass);
+        Model model = Model.getInstance();
+        model.addUser(user);
+
+        req.setAttribute("userName", name);
+        doGet(req, resp);
     }
 }
--- src/main/java/com/autopark/app/servlets/ListServlet.java
+++ src/main/java/com/autopark/app/servlets/ListServlet.java
@@ -1,16 +1,24 @@
 package com.autopark.app.servlets;
 
+import com.autopark.app.model.Model;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 
 public class ListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.println("Method GET from ListServlet");
+        req.setCharacterEncoding("UTF-8");
+        Model model = Model.getInstance();
+        List<String> names = model.list();
+        req.setAttribute("userNames", names);
+
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/list.jsp");
+        requestDispatcher.forward(req, resp);
     }
 }
